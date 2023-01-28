703ecff60410d3b6191c82c00bf5b38f75fe94d7
Scoozy <s.safanovich@gmail.com>
2023-01-28 14:44:56 -0800

    finished first part

--- src/layouts/dashboard/index.jsx
+++ src/layouts/dashboard/index.jsx
@@ -4,7 +4,9 @@ import {
   Divider,
   IconButton,
   Stack,
+  Switch,
   useTheme,
+  styled
 } from "@mui/material";
 import { Nav_Buttons } from "../../data";
 import React from "react";
@@ -13,10 +15,54 @@ import Logo from "../../assets/Images/logo.ico";
 import { Gear } from "phosphor-react";
 import { useState } from "react";
 import { faker } from "@faker-js/faker";
+import useSettings from '../../hooks/useSettings'
+
+const AntSwitch = styled(Switch)(({ theme }) => ({
+  width: 40,
+  height: 20,
+  padding: 0,
+  display: 'flex',
+  '&:active': {
+    '& .MuiSwitch-thumb': {
+      width: 15,
+    },
+    '& .MuiSwitch-switchBase.Mui-checked': {
+      transform: 'translateX(9px)',
+    },
+  },
+  '& .MuiSwitch-switchBase': {
+    padding: 2,
+    '&.Mui-checked': {
+      transform: 'translateX(20px)',
+      color: '#fff',
+      '& + .MuiSwitch-track': {
+        opacity: 1,
+        backgroundColor: theme.palette.mode === 'dark' ? '#177ddc' : '#1890ff',
+      },
+    },
+  },
+  '& .MuiSwitch-thumb': {
+    boxShadow: '0 2px 4px 0 rgb(0 35 11 / 20%)',
+    width: 16,
+    height: 16,
+    borderRadius: 8,
+    transition: theme.transitions.create(['width'], {
+      duration: 200,
+    }),
+  },
+  '& .MuiSwitch-track': {
+    borderRadius: 20 / 2,
+    opacity: 1,
+    backgroundColor:
+      theme.palette.mode === 'dark' ? 'rgba(255,255,255,.35)' : 'rgba(0,0,0,.25)',
+    boxSizing: 'border-box',
+  },
+}));
 const DashboardLayout = () => {
   const theme = useTheme();
   console.log(theme);
   const [selected, setSelected] = useState();
+  const {onToggleMode} = useSettings()
   return (
     <>
       <Box
@@ -75,7 +121,7 @@ const DashboardLayout = () => {
                     onClick={() => {
                       setSelected(el.index);
                     }}
-                    sx={{ width: "max-content", color: "black" }}
+                    sx={{ width: "max-content", color: theme.palette.mode === 'light'? '#000' : theme.palette.text.primary}}
                     key={el.index}
                   >
                     {el.icon}
@@ -100,15 +146,18 @@ const DashboardLayout = () => {
                   onClick={() => {
                     setSelected(3);
                   }}
-                  sx={{ width: "max-content", color: "black" }}
+                  sx={{ width: "max-content", color: theme.palette.mode === 'light'? '#000' : theme.palette.text.primary}}
                 >
                   <Gear />
                 </IconButton>
               )}
             </Stack>
           </Stack>
-          <Stack>
+          <Stack spacing={4}>
             {/* switch */}
+            <AntSwitch defaultChecked onChange={() => {
+              onToggleMode()
+            }}/>
             <Avatar src={faker.image.avatar()} />
           </Stack>
         </Stack>

445d062a3fd8e6f0ad42595c5c0e0be8f728dcf8
Scoozy <s.safanovich@gmail.com>
2023-01-28 13:53:03 -0800

    hi

--- src/layouts/dashboard/index.jsx
+++ src/layouts/dashboard/index.jsx
@@ -1,11 +1,22 @@
-import { Box, IconButton, Stack, useTheme } from "@mui/material";
-import {Nav_Buttons} from '../../data/index'
+import {
+  Avatar,
+  Box,
+  Divider,
+  IconButton,
+  Stack,
+  useTheme,
+} from "@mui/material";
+import { Nav_Buttons } from "../../data";
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Logo from "../../assets/Images/logo.ico";
+import { Gear } from "phosphor-react";
+import { useState } from "react";
+import { faker } from "@faker-js/faker";
 const DashboardLayout = () => {
   const theme = useTheme();
   console.log(theme);
+  const [selected, setSelected] = useState();
   return (
     <>
       <Box
@@ -17,24 +28,89 @@ const DashboardLayout = () => {
           width: 100,
         }}
       >
-        <Stack direction="column" alignItems={'center'} sx={{
-          width:"100%"
-        }}>
-          <Box
-            sx={{
-              backgroundColor: theme.palette.primary.main,
-              height: 64,
-              width: 64,
-              borderRadius: "12px ",
-            }}
-          >
-            <img src={Logo} alt="logo" />
-          </Box>
-          {Nav_Buttons.map((el) => {
-            <IconButton key={el.index}>
-              {el.icon}
-              
-          </IconButton>})}
+        <Stack
+          direction="column"
+          alignItems={"center"}
+          justifyContent={"space-between"}
+          sx={{
+            height: "100%",
+          }}
+          spacing={3}
+        >
+          <Stack alignItems="center" spacing={4}>
+            <Box
+              sx={{
+                backgroundColor: theme.palette.primary.main,
+                height: 64,
+                width: 64,
+                borderRadius: "12px ",
+              }}
+            >
+              <img src={Logo} alt="logo" />
+            </Box>
+            <Stack
+              spacing={3}
+              sx={{ width: "max-content" }}
+              direction="column"
+              alignItems="center"
+            >
+              {Nav_Buttons.map((el) =>
+                el.index === selected ? (
+                  <Box
+                    p={1}
+                    sx={{
+                      backgroundColor: theme.palette.primary.main,
+                      borderRadius: 1.5,
+                    }}
+                  >
+                    <IconButton
+                      sx={{ width: "max-content", color: "white" }}
+                      key={el.index}
+                    >
+                      {el.icon}
+                    </IconButton>
+                  </Box>
+                ) : (
+                  <IconButton
+                    onClick={() => {
+                      setSelected(el.index);
+                    }}
+                    sx={{ width: "max-content", color: "black" }}
+                    key={el.index}
+                  >
+                    {el.icon}
+                  </IconButton>
+                )
+              )}
+              <Divider sx={{ width: "48px" }} />
+              {selected === 3 ? (
+                <Box
+                  p={1}
+                  sx={{
+                    backgroundColor: theme.palette.primary.main,
+                    borderRadius: 1.5,
+                  }}
+                >
+                  <IconButton sx={{ width: "max-content", color: "white" }}>
+                    <Gear />
+                  </IconButton>
+                </Box>
+              ) : (
+                <IconButton
+                  onClick={() => {
+                    setSelected(3);
+                  }}
+                  sx={{ width: "max-content", color: "black" }}
+                >
+                  <Gear />
+                </IconButton>
+              )}
+            </Stack>
+          </Stack>
+          <Stack>
+            {/* switch */}
+            <Avatar src={faker.image.avatar()} />
+          </Stack>
         </Stack>
       </Box>
       <Outlet />
